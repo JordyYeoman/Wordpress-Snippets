ae7be3dee6bc7cf1e47ca71e8831451585fac95a
JordyYeoman <45798813+JordyYeoman@users.noreply.github.com>
2021-04-28 11:41:02 +0800

    Update Specific-Posts-Infinite-Loop.php

--- Specific-Posts-Infinite-Loop.php
+++ Specific-Posts-Infinite-Loop.php
@@ -2,21 +2,25 @@
 
 <?php
     if( get_adjacent_post(false, '', true) ) { 
-      previous_post_link('%link', '&larr; Previous Post');
+        $previous = get_previous_post();
+        previous_post_link('%link', '&larr; The Previous Hole ... <div>'. get_the_title($previous) . '</div>');
     } else { 
-      $first = new WP_Query('post_type=golf-hole&posts_per_page=1&order=DESC'); $first->the_post();
-        echo '<div>
-            <a href="' . get_permalink() . '">&larr; Previous Golf Hole</a>
-            </div>';
-      wp_reset_query();
+        $first = new WP_Query('post_type=golf-hole&posts_per_page=1&order=DESC'); $first->the_post();
+            echo '<div>
+                    <a href="' . get_permalink() . '">&larr; The Previous Holerino ...</a>
+                    <div>' . get_the_title() . '</div>
+                  </div>';
+        wp_reset_query();
     }; 
-    if( get_adjacent_post(false, '', false) ) { 
-      next_post_link('%link', 'Next Post &rarr;');
+    if( get_adjacent_post(false, '', false) ) {
+        $next = get_next_post();
+        next_post_link('%link', 'The Next Hole ... &rarr; <div>'. get_the_title($next) . '</div>');
     } else { 
-      $last = new WP_Query('post_type=golf-hole&posts_per_page=1&order=ASC'); $last->the_post();
-        echo '<div>
-            <a href="' . get_permalink() . '">Next Golf Hole &rarr;</a>
-           </div>';
-      wp_reset_query();
+        $last = new WP_Query('post_type=golf-hole&posts_per_page=1&order=ASC'); $last->the_post();
+            echo '<div>
+                    <a href="' . get_permalink() . '">The Next Hole ... &rarr;</a>
+                    <div>' . get_the_title() . '</div>
+                 </div>';
+        wp_reset_query();
     }; 
 ?>
